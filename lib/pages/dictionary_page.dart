eeddaa2dfb45c32b611253a4db7b2b09c15e82e4
Baitur5 <baibaitur@gmail.com>
2022-06-08 11:28:52 +0600

    Bug fix in dictionary info page

--- lib/pages/dictionary_page.dart
+++ lib/pages/dictionary_page.dart
@@ -3,6 +3,7 @@ import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:tmlearn/global_variables.dart';
 import 'package:tmlearn/hive_data/data.dart';
 import 'package:tmlearn/pages/dictionary_info_page.dart';
@@ -47,9 +48,72 @@ class _DictionaryPageState extends State<DictionaryPage> {
     print(show_find);
     return Scaffold(
       backgroundColor: backgroundColor,
-      appBar: const CustomTransparentAppBar(
-        titleRus: 'Словарь\n',
-        titleTurk: 'Sözlük',
+      // appBar: const CustomTransparentAppBar(
+      //   titleRus: 'Словарь\n',
+      //   titleTurk: 'Sözlük',
+      // ),
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Colors.transparent,
+        centerTitle: true,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 17.0),
+          child: FloatingActionButton(
+            heroTag: 'btn-back',
+            backgroundColor: Colors.black,
+            onPressed: () {
+              if (is_inside_view_word) {
+                setState(() {
+                  is_inside_view_word = !is_inside_view_word;
+                });
+              } else {
+                Navigator.pop(context);
+              }
+            },
+            child: Padding(
+              padding: const EdgeInsets.only(right: 3.0),
+              child: Icon(
+                Icons.arrow_back_ios_rounded,
+                color: primaryColor,
+              ),
+            ),
+          ),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 17.0),
+            child: FloatingActionButton.small(
+              heroTag: 'btn-drawer',
+              backgroundColor: Colors.black,
+              disabledElevation: 0,
+              onPressed: () => Scaffold.of(context).openEndDrawer(),
+              child: CircleAvatar(
+                backgroundColor: Colors.black,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Image.asset(
+                    "assets/TL.png",
+                    fit: BoxFit.fill,
+                  ),
+                ),
+              ),
+            ),
+          )
+        ],
+        title: RichText(
+          textAlign: TextAlign.center,
+          text: TextSpan(
+            style: GoogleFonts.nunito(
+              textStyle: const TextStyle(fontSize: 25.0),
+            ),
+            children: [
+              TextSpan(
+                text: "Словарь",
+                style: TextStyle(color: primaryColor),
+              ),
+            ],
+          ),
+        ),
       ),
       endDrawer: const NavigationDrawerWidget(),
       body: Padding(
